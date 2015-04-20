e008c7ea7d87a8306cf200c5c082367f919e8571
Luciano Quaranta <Luciano_40@hotmail.it>
2015-04-20 19:55:02 +0200

    Update MostraParametri.java
    aggiungo ancora una volta un commento

--- src/it/uniroma3/servelt/MostraParametri.java
+++ src/it/uniroma3/servelt/MostraParametri.java
@@ -17,7 +17,7 @@ public class MostraParametri extends HttpServlet {
 			throws ServletException, IOException {
 		// gestione della RICHIESTA
 		
-	
+		//aggiunto ancora una volta un commento
 		// leggo i parametri
 		String nome = request.getParameter("nome");
 		String cognome = request.getParameter("cognome");
